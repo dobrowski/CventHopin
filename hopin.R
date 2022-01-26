3fbfe11434d2f244572c0c25ddc5612bd56cccd0
David Dobrowski <dobrowski@gmail.com>
2022-01-25 16:09:59 -0800

    Comparing to prior iterations and building in file management

--- hopin.R
+++ hopin.R
@@ -4,15 +4,28 @@ library(tidyverse)
 library(here)
 library(janitor)
 
-cvent <- read_csv(here("new", "Registration Report_CISC 2022.csv"))
+
+recent.new <- file.info(list.files(here("new"), full.names = T))
+newest.file <- rownames(recent.new)[which.max(recent.new$mtime)]
+
+recent.old <- file.info(list.files(here("old"), full.names = T))
+newest.old.file <- rownames(recent.old)[which.max(recent.old$mtime)]
+  
+
+
+# cvent <- read_csv(here("new", "Registration Report_CISC 2022.csv"))
+new.file <- read_csv(newest.file)
+old.file <- read_csv(newest.old.file)
+
+cvent <- anti_join(new.file,old.file)
 
 
 hopin <- cvent %>%
   transmute(Email = `Email Address`,
          FirstName = `First Name`,
          LastName = `Last Name`,
-         Headline = Title,
-         `optional question 1` = `Company Name`,
+         Headline = paste0(Title," - ",`Company Name`),
+         `optional question 1` = `Confirmation Number`,
          `optional question 2` = `What is your CCSESA Region? If unsure, please visit: https://ccsesa.org/regions/`,
          ticket = case_when(str_detect(`Registration Type`,"Sponsor") ~ "Sponsors",
                             str_detect(`Registration Type`,"Invited") ~ "Speakers",
@@ -26,4 +39,13 @@ hopin <- cvent %>%
 
 hopin %>% 
   group_by(ticket) %>% 
-  group_walk(~ write_csv(.x, paste0(.y$ticket, ".csv")))
+  group_walk(~ write_csv(.x, here("to_upload", paste0(.y$ticket, Sys.time() ,".csv"))))
+
+
+#  Add date to file 
+#  Deduplicate from previous list first 
+
+file.rename(from = newest.file,
+            to = here("old", paste0("Registration Report_CISC 2022", Sys.time() ,".csv"))
+)
+
